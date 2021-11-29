a4617be39238f5293b45c185afd9c798ed0ed3c2
choang9 <choang@seattleu.edu>
2021-11-28 16:35:28 -0800

    Added routes and views

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express();
+const config = require('./config');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+const port = 3000;
+const authentication = `${config.EMAIL}/token:${config.API_KEY}`;
+let buff = Buffer.from(authentication);
+let base64data = buff.toString('base64');
+const url = 'https://zcchoang.zendesk.com/api/v2/tickets.json';
+
+app.get('/', (req, res) => {
+    fetch(url, {
+        headers: {
+            Authorization: 'Basic ' + base64data,
+        },
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            res.render('index', { tickets: data.tickets });
+        })
+        .catch((err) => {
+            res.render('error', {
+                errorMessage: err,
+            });
+        });
+});
+
+app.get('/ticket/:id', (req, res) => {
+    fetch(url, {
+        headers: {
+            Authorization: 'Basic ' + base64data,
+        },
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            const ticket = data.tickets.find(
+                (ticket) => ticket.id == req.params.id
+            );
+            res.render('details', { ticket: ticket });
+        })
+        .catch((err) => {
+            res.render('error', {
+                errorMessage: err,
+            });
+        });
+});
+
+const server = app.listen(port);
